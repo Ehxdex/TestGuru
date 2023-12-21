66e1a9bcd3fc1a45835f24acf7e035fd39643d96
rav <reznichenkoav@me.com>
2023-12-21 12:14:08 +0300

    Feat: user model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_12_21_091302) do
+ActiveRecord::Schema[7.0].define(version: 2023_12_21_091350) do
   create_table "answers", force: :cascade do |t|
     t.string "body"
     t.datetime "created_at", null: false
@@ -26,6 +26,13 @@ ActiveRecord::Schema[7.0].define(version: 2023_12_21_091302) do
   create_table "tests", force: :cascade do |t|
     t.string "title"
     t.integer "level"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "name"
+    t.string "email"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
