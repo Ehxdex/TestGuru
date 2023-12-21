7aa1fe0273ae5cfdf1b537e06c9879c81c0cb811
rav <reznichenkoav@me.com>
2023-12-21 12:14:58 +0300

    Feat: category model

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_12_21_091350) do
+ActiveRecord::Schema[7.0].define(version: 2023_12_21_091443) do
   create_table "answers", force: :cascade do |t|
     t.string "body"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "categories", force: :cascade do |t|
+    t.string "title"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
