42c93f478dce4a922a768354ba3dd560d426ddf3
rav <reznichenkoav@me.com>
2023-12-21 14:34:50 +0300

    Fix users null constraint

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_12_21_112635) do
+ActiveRecord::Schema[7.0].define(version: 2023_12_21_113307) do
   create_table "answers", force: :cascade do |t|
     t.string "body", null: false
     t.datetime "created_at", null: false
@@ -37,8 +37,8 @@ ActiveRecord::Schema[7.0].define(version: 2023_12_21_112635) do
   end
 
   create_table "users", force: :cascade do |t|
-    t.string "name"
-    t.string "email"
+    t.string "name", null: false
+    t.string "email", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
