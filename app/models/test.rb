8f9056316cec44b86e5ae6447053a059c225de2a
rav <reznichenkoav@me.com>
2024-01-08 20:19:10 +0300

    Feat: class method sort_by(category) test.rb

--- app/models/test.rb
+++ app/models/test.rb
@@ -2,4 +2,8 @@ class Test < ApplicationRecord
   belongs_to :category
   belongs_to :user
   has_many :questions
+
+  def self.sort_by(category)
+    Test.where(categories: { title: category}).joins(:category).order(title: :DESC).pluck(:title)
+  end
 end
