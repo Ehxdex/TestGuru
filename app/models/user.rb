a3610c4ddfd8f9d1762e6aea16a6ac04174e1f27
rav <reznichenkoav@me.com>
2024-01-11 15:42:06 +0300

    Fix: instance method 'tests_by' in users model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,6 @@
 class User < ApplicationRecord
   def tests_by(level)
-    Test.where(level: level)
+    Test.joins("JOIN results ON results.test_id = tests.id")
+      .where(level: level, results: { author_id: id })
   end
 end
