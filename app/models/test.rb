cef0dde1cb110409dbe50a167b55efb6c485fa29
rav <reznichenkoav@me.com>
2024-01-18 16:33:17 +0300

    Feat: associations to existing models

--- app/models/test.rb
+++ app/models/test.rb
@@ -1,4 +1,10 @@
 class Test < ApplicationRecord
+  belongs_to :category
+  has_many :questions
+  has_many :results
+  has_many :users, through: :results
+  belongs_to :author, class_name: 'User'
+
   def self.titles_by_category(category)
     joins("JOIN categories ON tests.category_id = categories.id")
       .where(categories: { title: category })
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,8 @@
 class User < ApplicationRecord
+  has_many :results
+  has_many :tests, through: :results
+  has_many :tests_author, class_name: 'Test', foreign_key: 'author_id'
+
   def passed_tests_by_level(level)
     Test.joins("JOIN results ON results.test_id = tests.id")
       .where(level: level, results: { user_id: id })
