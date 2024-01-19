b96515e16942fd713c242fc75f557b2ca7aa3e09
rav <reznichenkoav@me.com>
2024-01-19 20:16:26 +0300

    Fix: association behavior when deleted

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ApplicationRecord
-  has_many :results
-  has_many :tests, through: :results
-  has_many :tests_author, class_name: 'Test', foreign_key: 'author_id'
+  has_many :results, dependent: :destroy
+  has_many :tests, through: :results, dependent: :destroy
+  has_many :tests_author, class_name: 'Test', foreign_key: 'author_id', dependent: :destroy
 
   def passed_tests_by_level(level)
     Test.joins(:results)
