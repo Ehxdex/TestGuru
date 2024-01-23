af87318d43803f0667ff44f374cefef1a108348c
rav <reznichenkoav@me.com>
2024-01-23 11:07:32 +0300

    Feat: scope-methods

--- app/models/test.rb
+++ app/models/test.rb
@@ -6,10 +6,14 @@ class Test < ApplicationRecord
   has_many :results, dependent: :destroy
   has_many :users, through: :results, dependent: :destroy
 
-  def self.titles_by_category(category)
+  scope :easy_tests_by_level, -> { where(level: 0..1) }
+  scope :medium_tests_by_level, -> { where(level: 2..4) }
+  scope :hard_tests_by_level, -> { where(level: 5..Float::INFINITY) }
+
+  scope :titles_by_category, ->(category) { 
     joins(:category)
-      .where(categories: { title: category })
+      .where(categories: { title: category }) 
       .order(title: :desc)
       .pluck(:title)
-  end
+  }
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,5 @@ class User < ApplicationRecord
   has_many :tests, through: :results, dependent: :destroy
   has_many :tests_author, class_name: 'Test', foreign_key: 'author_id', dependent: :destroy
 
-  def passed_tests_by_level(level)
-    tests.where(level: level)
-  end
+  scope :passed_tests_by_level, ->(level) { tests.where(level: level) }
 end
