59493e2220551124dac94a1ffda5419741a75ce7
rav <reznichenkoav@me.com>
2024-02-05 11:03:33 +0300

    Feat: question controller rescue (question non found)

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,6 +1,8 @@
 class QuestionsController < ApplicationController
   before_action :find_test, only: [:index, :create]
   before_action :find_question, only: [:show, :destroy]
+
+  rescue_from ActiveRecord::RecordNotFound, with: :record_question_not_found
   
   def index 
   end
@@ -19,22 +21,25 @@ class QuestionsController < ApplicationController
 
   def destroy
     @question.destroy
-    flash[:success] = "Question deleted"
 
     redirect_to test_questions_path(@question.test.id)
   end
 
   private
 
-  def find_test
-    @test = Test.find(params[:test_id])
-  end
+    def find_test
+      @test = Test.find(params[:test_id])
+    end
 
-  def find_question
-    @question = Question.find(params[:id])
-  end
-  
-  def question_params
-    params.require(:question).permit(:body)
-  end
+    def find_question
+      @question = Question.find(params[:id])
+    end
+    
+    def question_params
+      params.require(:question).permit(:body)
+    end
+
+    def record_question_not_found
+      render plain: "Question Not Found"
+    end
 end
