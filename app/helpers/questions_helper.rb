3c9b344431a2751ad6e52b64eef2d57e06949ddd
rav <reznichenkoav@me.com>
2024-02-16 08:39:51 +0300

    Feat: questions helper methods

--- app/helpers/questions_helper.rb
+++ app/helpers/questions_helper.rb
@@ -1,2 +1,13 @@
 module QuestionsHelper
+  def question_header
+    "<#{@test.title}>"
+  end
+
+  def current_year
+    Time.now.year
+  end
+
+  def github_url(author, repo)
+    "https://github.com/#{author}/#{repo}"
+  end
 end
