4c950efb8ad8766ff14bde9e7b42791e27d75418
rav <reznichenkoav@me.com>
2024-02-18 14:15:59 +0300

    Feat: Tests controller methods, Tests views (index, show, edit)

--- app/controllers/tests_controller.rb
+++ app/controllers/tests_controller.rb
@@ -1,5 +1,59 @@
 class TestsController < ApplicationController
+  before_action :find_test, only: [:show, :edit, :update, :destroy]
+
+  rescue_from ActiveRecord::RecordNotFound, with: :record_test_not_found
+
   def index
     @tests = Test.all
+  end
+
+  def show
+  end
+
+  def new
+    @test = Test.new
+  end
+
+  def create
+    @test = Test.new(test_params)
+
+    @test.author = User.first
+
+    if @test.save
+      redirect_to tests_path
+    else
+      render :new
+    end
+  end
+
+  def edit
+  end
+
+  def update
+    if @test.update(test_params)
+      redirect_to tests_path
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @test.destroy
+
+    redirect_to tests_path
+  end
+
+  private
+
+  def find_test
+    @test = Test.find(params[:id])
+  end
+
+  def test_params
+    params.require(:test).permit(:title, :level, :category_id)
+  end
+
+  def record_test_not_found
+    render plain: "Test Not Found"
   end
 end
--- app/helpers/questions_helper.rb
+++ app/helpers/questions_helper.rb
@@ -2,12 +2,4 @@ module QuestionsHelper
   def question_header
     "<#{@test.title}>"
   end
-
-  def current_year
-    Time.now.year
-  end
-
-  def github_url(author, repo)
-    "https://github.com/#{author}/#{repo}"
-  end
 end
