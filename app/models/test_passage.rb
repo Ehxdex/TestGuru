6cd88ee9adc920deaa54a4595628259668f074cf
rav <reznichenkoav@me.com>
2024-04-19 12:19:08 +0300

    Feat: test_passages mvc, routes, main methods

--- app/models/test_passage.rb
+++ app/models/test_passage.rb
@@ -2,4 +2,34 @@ class TestPassage < ApplicationRecord
   belongs_to :user
   belongs_to :test
   belongs_to :current_question, class_name: 'Question', optional: true
+
+  def completed?
+    current_question.nil?
+  end
+  
+  def accept!(answer_ids)
+    if correct_answer?
+      self.correct_question += 1
+    end
+  
+    self.current_question = next_question
+    save!
+  end
+  
+  private
+  
+  def correct_answer?(answer_ids)
+    correct_answers_count = correct_answers.count
+  
+    (correct_answers_count == correct_answers.where(id: answer_ids).count) &&
+    correct_answers_count == answer_ids.count
+  end
+  
+  def correct_answers
+    current_question.answers.correct
+  end
+  
+  def next_question
+    test.questions.order(:id).where('id > ?', current_question.id).first
+  end
 end
