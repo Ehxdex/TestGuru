482fb8ccb9e001936069bcd3633eed76e643a99a
rav <reznichenkoav@me.com>
2024-04-19 11:04:09 +0300

    Feat: Cotroller test_passages, fix: user & test associations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ApplicationRecord
-  has_many :results, dependent: :destroy
-  has_many :tests, through: :results, dependent: :destroy
+  has_many :test_passages, dependent: :destroy
+  has_many :tests, through: :test_passages, dependent: :destroy
   has_many :tests_author, class_name: 'Test', foreign_key: 'author_id', dependent: :destroy
 
   validates :name, length: { minimum: 2 }, presence: true
