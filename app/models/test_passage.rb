957ed5c2af5b5e7b035a9872858a02d5b8fa62a1
rav <reznichenkoav@me.com>
2024-04-24 16:50:31 +0300

    Feat: TestPassage callbacks, fix: some errors

--- app/models/test_passage.rb
+++ app/models/test_passage.rb
@@ -3,26 +3,33 @@ class TestPassage < ApplicationRecord
   belongs_to :test
   belongs_to :current_question, class_name: 'Question', optional: true
 
+  before_validation :before_validation_set_first_question, on: :create
+  before_validation :before_validation_set_next_question, on: :update
+
   def completed?
     current_question.nil?
   end
   
   def accept!(answer_ids)
-    if correct_answer?
-      self.correct_question += 1
+    if correct_answer?(answer_ids)
+      self.correct_questions += 1
     end
-  
-    self.current_question = next_question
+    
     save!
   end
   
   private
+
+  def before_validation_set_first_question
+    self.current_question = test.questions.first if test.present?
+  end
+  
+  def before_validation_set_next_question
+    self.current_question = next_question
+  end
   
   def correct_answer?(answer_ids)
-    correct_answers_count = correct_answers.count
-  
-    (correct_answers_count == correct_answers.where(id: answer_ids).count) &&
-    correct_answers_count == answer_ids.count
+    correct_answers.ids.sort == answer_ids.to_a.map(&:to_i).sort
   end
   
   def correct_answers
