3a42ed2b394aaf989e141d06da3c8f2519bdc194
rav <reznichenkoav@me.com>
2024-05-20 12:21:35 +0300

    Feat: test mailer, fix: seed.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -13,7 +13,7 @@ Test.destroy_all
 Category.destroy_all
 User.destroy_all
 
-users = User.create!([name: 'User1', email: 'test1@test.com'])
+users = User.create!([name: 'User1', email: 'test1@test.com', password: 'iamaguru', password_confirmation: 'iamaguru'])
 
 categories = Category.create!([
   { title: 'Ruby' },
