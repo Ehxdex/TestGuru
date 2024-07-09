30f7d0bedf618f4960b87d7ebb467c79eb1fa7d9
rav <reznichenkoav@me.com>
2024-07-09 23:34:42 +0300

    Fix: action mailer syntax error

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -63,7 +63,7 @@ Rails.application.configure do
   # config.active_job.queue_name_prefix = "TestGuru_production"
 
   config.action_mailer.perform_caching = false
-  config.action_mailer.default_url_option = { host: 'https://testguru-ekxf.onrender.com' }
+  config.action_mailer.default_url_options = { host: 'https://testguru-ekxf.onrender.com' }
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
     address:              'smtp.gmail.com',
