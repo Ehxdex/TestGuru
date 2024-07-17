4f5b945c41ad1de7cd2528f5a43bd80d48fc146a
rav <reznichenkoav@me.com>
2024-07-17 13:52:43 +0300

    Feat: feedback controller new create methods

--- app/controllers/feedbacks_controller.rb
+++ app/controllers/feedbacks_controller.rb
@@ -1,7 +1,22 @@
 class FeedbacksController < ApplicationController
   def new
+    @feedback = Feedback.new
   end
 
   def create
+    @feedback = Feedback.new(freedback_params)
+  
+    if @feedback.save
+      # send message
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def freedback_params
+    params.require(:feedback).permit(:name, :email, :message)
   end
 end
